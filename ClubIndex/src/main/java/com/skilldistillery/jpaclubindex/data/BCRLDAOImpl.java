879573560c0a14af8c0a191673cdca703e5487cb
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-10-24 09:54:56 -0500

    Fixed BCRLDAOImpl

--- ClubIndex/src/main/java/com/skilldistillery/jpaclubindex/data/BCRLDAOImpl.java
+++ ClubIndex/src/main/java/com/skilldistillery/jpaclubindex/data/BCRLDAOImpl.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.jpaclubindex.data;
 
-public class BCRLDAOImpl implements BCRLDAO{
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class BCRLDAOImpl implements BCRLDAO{
+	
+	@PersistenceContext
+	EntityManager em;
 }
