d3b6f428afcb503da13e5d2b2e104ca612a7fbb8
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-10-24 14:26:53 -0500

    Finished BookClubDAOImpl

--- ClubIndex/src/main/java/com/skilldistillery/jpaclubindex/data/BookClubDAOImpl.java
+++ ClubIndex/src/main/java/com/skilldistillery/jpaclubindex/data/BookClubDAOImpl.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.jpaclubindex.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -27,50 +28,99 @@ public class BookClubDAOImpl implements BookClubDAO{
 
 	@Override
 	public BookClub getBookClubById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(BookClub.class, id);
 	}
 
 	@Override
 	public List<BookClub> getBookClubsByOpenMembers() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc";
+		
+		List<BookClub> allBc = em.createQuery(query, BookClub.class)
+				.getResultList();
+		
+		List<BookClub> returnList = new ArrayList<>();
+		
+		allBc.stream().forEach( (BookClub bc) -> {
+			if(bc.getMaxMembers() - bc.getUsers().size() > 0) {
+				returnList.add(bc);
+			}
+		});
+		
+		return returnList;
 	}
 
 	@Override
 	public List<BookClub> getBookClubsByDigital(boolean isDigital) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc WHERE bc.digital = :isDigital";
+		
+		return em.createQuery(query, BookClub.class).setParameter("isDigital", isDigital).getResultList();
 	}
 
 	@Override
 	public List<BookClub> getAllBookClubs() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc";
+		return em.createQuery(query, BookClub.class).getResultList();
 	}
 
 	@Override
 	public BookClub getBookClubByReadingList(BookClubReadingList bcrl) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc WHERE :bcrl MEMBER OF bc.readingLists";
+		return em.createQuery(query, BookClub.class).setParameter("bcrl", bcrl).getSingleResult();
 	}
 
 	@Override
 	public List<BookClub> getBookClubsByOwner(User owner) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc WHERE bc.owner = :owner";
+		return em.createQuery(query, BookClub.class).setParameter("owner", owner).getResultList();
 	}
 
 	@Override
 	public List<BookClub> getBookClubsByGenre(Genre genre) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT bc FROM BookClub bc WHERE :genre MEMBER OF bc.genres";
+		return em.createQuery(query, BookClub.class).setParameter("genre", genre).getResultList();
 	}
 
 	@Override
 	public List<BookClub> getBookClubsByLocation(Location location) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public BookClub createBookClub(BookClub bc) {
+		em.persist(bc);
+		em.flush();
+		return bc;
+	}
+
+	@Override
+	public boolean removeBookClub(BookClub bc) {
+		em.remove(bc);
+		return !em.contains(bc);
+	}
+
+	@Override
+	public BookClub updateBookClub(BookClub bcOld, BookClub bcNew) {
+		bcOld = em.find(BookClub.class, bcOld.getId());
+		bcOld = bcNew;
+		em.flush();
+		return bcOld;
+	}
+
+	@Override
+	public BookClub addUserToBookClub(BookClub bc, User user) {
+		bc = em.find(BookClub.class, bc.getId());
+		bc.addUser(user);
+		em.flush();
+		return bc;
+	}
+
+	@Override
+	public BookClub removeUserFromBookClub(BookClub bc, User user) {
+		bc = em.find(BookClub.class, bc.getId());
+		bc.removeUser(user);
+		em.flush();
+		return bc;
 	}
 
 }
