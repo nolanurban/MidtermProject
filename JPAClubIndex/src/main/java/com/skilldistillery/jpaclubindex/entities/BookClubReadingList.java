c58c20e73c94632653eaac6aa6d9eaad4d1dd91c
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-10-27 14:14:15 -0500

    Cleaned imports of JPA project

--- JPAClubIndex/src/main/java/com/skilldistillery/jpaclubindex/entities/BookClubReadingList.java
+++ JPAClubIndex/src/main/java/com/skilldistillery/jpaclubindex/entities/BookClubReadingList.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,7 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.LazyCollection;
